67c75b4b1ad51065b72eb1678db1f84f5bd4e22d
maxwellvolz <maxwellvolz@gmail.com>
2015-12-23 23:09:26 -0800

    now time stamping!

---
+++ scripts/app2.js
@@ -0,0 +1,30 @@
+
+
+// Prompt the user for a name to use.
+var name = prompt("Your name?", "Guest");
+var currentStatus = "online";
+var app = angular.module('presence', ['firebase']);
+
+var amOnline = new Firebase('https://pr3z.firebaseio.com/.info/connected');
+var userRef = new Firebase('https://pr3z.firebaseio.com/presence/' + name);
+var ref = new Firebase('https://pr3z.firebaseio.com/presence');
+
+
+amOnline.on('value', function(snapshot) {
+  if (snapshot.val()) {
+    var sessionRef = userRef.push();
+    sessionRef.child('ended').onDisconnect().set(Firebase.ServerValue.TIMESTAMP);
+    sessionRef.child('began').set(Firebase.ServerValue.TIMESTAMP);
+  }
+});
+
+app.controller('LobbyCtrl', 
+  function($scope, $firebase) {
+
+    var obj = $firebase(ref).$asObject();
+
+
+    obj.$bindTo($scope, "data");
+    console.log(obj);
+
+  });
