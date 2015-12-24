9d2d9b25b22dedab63a9fa4f2acf472e6cfb355a
maxwellvolz <maxwellvolz@gmail.com>
2015-12-24 02:20:43 -0800

    placeholders for button events

--- scripts/app2.js
+++ scripts/app2.js
@@ -21,19 +21,41 @@ amOnline.on('value', function(snapshot) {
 app.controller('LobbyCtrl', 
   function($scope, $firebase) {
 
+    // get database and bind it to dom
     var obj = $firebase(ref).$asObject();
-
     obj.$bindTo($scope, "data");
-    console.log(obj);
 
-});
+    // keypress handlings
+    $scope.handleKeyup = function handleKeyup(e) {
+      if (e.keyCode == 65) {
+        console.log("A(65) was pressed.");
+
+      }
+      if (e.keyCode == 68) {
+        console.log("D(68) was pressed.");
+
+      }
+    }
+
+    $scope.create = function() {
+        console.log("Create button pressed.")
+    };
+    $scope.reset = function() {
+        console.log("Reset button pressed.")
+    };
+    $scope.delete = function() {
+        console.log("Delete button pressed.")
+    };
+
+
+  });
 
 
 app.filter('timeAgo', function() {
-    return function(input) {
-        if (input == null) return "";
-        return jQuery.timeago(input);
-    };
+  return function(input) {
+    if (input == null) return "";
+    return jQuery.timeago(input);
+  };
 });
 
 app.filter('statusClass', function(){
